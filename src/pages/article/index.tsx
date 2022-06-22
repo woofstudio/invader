6b645e1fb2cf0d760e5eca5d148e1a1f67927627
Ebonian <tanadon.santisan@gmail.com>
2022-06-22 21:04:46 +0700

    feat: pages

---
+++ src/pages/article/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Articles: NextPage = () => {
+  return <div>Articles</div>;
+};
+
+export default Articles;
---
+++ src/pages/promotion.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Promotion: NextPage = () => {
+  return <div>Promotion</div>;
+};
+
+export default Promotion;
