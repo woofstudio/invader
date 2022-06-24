5bae6c4bb98dce6ef4e0d7aa674cdd3ba152af8a
soponkntg <sopon-1231@hotmail.com>
2022-06-24 23:06:43 +0700

    seo and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="th">
       <Head>
         {/* import font */}
         <link
@@ -20,6 +20,23 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" key="apple" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+          key="icon32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+          key="icon16"
+        />
+        <link rel="icon" href="/favicon.ico" key="favicon" />
       </Head>
       <body className="bg-dark-100">
         <Main />
