c7f3ac052be6f4d0317f9411339e9e376d198139
woofstudio <107114371+woofstudio@users.noreply.github.com>
2022-07-17 21:26:37 +0700

    Update sanity.ts

--- src/sanity.ts
+++ src/sanity.ts
@@ -10,6 +10,7 @@ import {
   createPreviewSubscriptionHook,
 } from 'next-sanity'
 
+
 const config: ClientConfig = {
   /**
    * Find your project ID and dataset in `sanity.json` in your studio project.
